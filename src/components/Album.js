4cb5aeca79d60bf451603bd4b2cd5b43772d6b36
8982osh <cruztoyota@gmail.com>
2019-01-16 22:37:27 -0600

    Checkpoint audio playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -52,6 +53,15 @@ class Album extends Component {
 
   offHover(song){
     this.setState({isHovered: false});
+  }
+
+  /* find previous song */
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
   }
 
   /* change icons for hover on and off */
@@ -108,6 +118,12 @@ class Album extends Component {
             }
   	       </tbody>
   	     </table>
+         <PlayerBar 
+           isPlaying={this.state.isPlaying} 
+           currentSong={this.state.currentSong} 
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+         />
   	  </section>
   	);
   }
